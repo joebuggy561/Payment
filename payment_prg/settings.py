24b7dc24af49df2244954b519d91561d9f71d58e
Joseph <60448358+joebuggy561@users.noreply.github.com>
2023-09-26 16:49:43 +0400

    third commit

--- payment_prg/settings.py
+++ payment_prg/settings.py
@@ -82,12 +82,12 @@ WSGI_APPLICATION = 'payment_prg.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 # DATABASES = {
 #     'default': {
 #         'ENGINE': os.environ.get("ENGINE"),
